1421aa4255c1db02eda8e21bddcdaf82cf458ba6
Jackson Chui <14085209+jacksonchui@users.noreply.github.com>
2021-03-05 17:19:00 -0800

    Added Windows support for the prompt.

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 ### Chapter 4
 
 To use `editline`, include `<editline/readline.h>` and link and compile with the following: `cc --std=c99 -o prompt prompt.c -ledit`. On macOS, adding `ledit` to the linker command adds the `libedit.dylib`, which comes with GNU readline and history libraries and is default on the mac.
+
+Adding `-g` as an arg allows you to use *lldb* or *gdb* in order to debug your program.
--- prompt.c
+++ prompt.c
@@ -1,27 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <editline/readline.h>
+
+#ifdef _WIN32
+#include <string.h>
 
 static char input[2048];
 
+char* readline(char* prompt) {
+    fputs(prompt, stdout);
+    fgets(buffer, 2048, stdin);
+    char* cpy = malloc(strlen(buffer) + 1);
+
+    /* doesn't copy the null charater */
+    strcpy(cpy, buffer); 
+    cpy(strlen(cpy) - 1) = '\0';
+    return cpy;
+}
+
+/* Fake add_history function */
+void add_history(char* unussed) { }
+/*
+# elif __APPLE__
+    #include <TargetConditionals.h>
+    #if TARGET_IPHONE_SIMULATOR 
+        // iOS Simulator
+    #elif TARGET_OS_IPHONE
+        // iOS Device
+    #elif Target_OS_MAC
+        // Other kinds of macOS
+    #endif
+*/
+#elif __linux__
+    printf("Using a Linux Platform")
+#elif __unix__
+#else
+#include <editline/readline.h>
+#endif
+
 int main(int argc, char** argv) {
     
-    puts("Lispy Version 0.0.0.0.1");
+    puts("LispC Version chapter 4");
     puts("Press Ctrl+c to Exit\n");
 
     while (1) {
-        
-        // fputs("lispy> ", stdout);
-        char* input = readline("lispy> ");
-        
-        // fgets(input, 2048, stdin);
+
+        /* Readline is defined properly on either platform */
+        char* input = readline("lispC> ");
         add_history(input);
         
-        /* Echo input back to user */
-        printf("No you're a %s", input);
-        
-        /* Free the retrieved input */
+        printf("No you're a %s\n", input);
         free(input);
     }
     
